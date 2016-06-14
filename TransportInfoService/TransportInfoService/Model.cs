598d06f6acb7e54b6b41938f7183274de8249774
unknown <admin@A2-7.itstep.lan>
2016-06-14 19:04:23 +0300

    Vadim commit 1

---
+++ TransportInfoService/TransportInfoService/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportInfoService
+{
+    public static class Model
+    {
+
+    }
+}
